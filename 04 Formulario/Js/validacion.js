61aca9c278efa3355ed8437decf5c1bfbac5ba8e
HernandezCarmonaLuisEmiliano <lhernandezc2306@alumno.ipn.mx>
2025-10-11 13:52:42 -0600

    form

--- 04 Formulario/Js/validacion.js
+++ 04 Formulario/Js/validacion.js
@@ -1,28 +1,70 @@
 /*Javascript es un lenguaje multiparadigma Acepta la programacion funcional, estructurada POO, y a eventos Dentro de Js, no existe el typado de variables int, string, float, etc. Solo existen 3 tipos de variables de acuerdo al estandar ES6: VAR, LET, CONST */
 function validar(formulario){
     if(formulario.nombre.value.length < 4){
-        alert("Porfavor escriba bien");
+        alert("Por favor escribe mas de 3 caracteres en campo nombre");
         formulario.nombre.focus();
         return false;
     }
-    var checkstr = formulario.nombre.value;
-    alert(checkstr);
-    var abcok="QWERTYUIOPASDFGHJKLÑZXCVBNMqwertyuiopasdfghjklñzxcvbnm";
+
+    var checkStr = formulario.nombre.value;
+    alert(checkStr);
+
+    var abcOk = "QWERTYUIOPASDFGHJKLÑZXCVBNM"+"qwertyuiopasdfghjklñzxcvbnm";
+
     var allValido = true;
 
-    for(var i=0; i < checkstr.length; i++){
-        var caracteres = checkstr.charAt(i);
-        for(var j=0;j<abcok.length;j++){
-            if(caracteres == abcok.charAt(j)) break;
+    for(var i = 0; i < checkStr.length; i++){
+        var caracteres = checkStr.charAt(i);
+        for(var j = 0; j < abcOk.length; j++){
+            if(caracteres == abcOk.charAt(j)){
+                break;
+            }
         }
-        if(j==abcok.length){
-            allValido=false;
+        if(j == abcOk.length){
+            allValido = false;
             break;
         }
     }
     if(!allValido){
-        alert("Escriba unicamente letras en el nombre")
+        alert("Escriba unicamente letras en el campo nombre");
         formulario.nombre.focus();
         return false;
     }
+
+
+    var checkStr = formulario.edad.value;
+    alert(checkStr);
+
+    var abcOk = "1234567890";
+
+    var allValido = true;
+
+
+    for(var i = 0; i < checkStr.length; i++){
+        var caracteres = checkStr.charAt(i);
+        for(var j = 0; j < abcOk.length; j++){
+            if(caracteres == abcOk.charAt(j)){
+                break;
+            }
+        }
+        if(j == abcOk.length){
+            allValido = false;
+            break;
+        }
+    }
+    if(!allValido){
+        alert("Escriba unicamente digitos en el campo nombre");
+        formulario.edad.focus();
+        return false;
+    }
+
+
+    var b = /^[^@\s]+[^@\.\s]+(\.[^@\.\s]+)+$/;
+
+    var txt = formulario.correo.value;
+
+    alert("Email " + (b.test(txt)? " ": " no ")+ "valido");
+
+    return b.test;
+
 }
