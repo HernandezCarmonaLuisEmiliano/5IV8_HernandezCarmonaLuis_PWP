5efc8de20553468bb0570950bc5450ddf9b6bd14
HernandezCarmonaLuisEmiliano <lhernandezc2306@alumno.ipn.mx>
2025-11-08 00:33:57 -0600

    Practica 10

---
+++ 10Postman/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mirrow = require('./endpoint/mirrow');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json()); 
+app.get('/', mirrow);
+app.post('/', mirrow);
+app.put('/', mirrow);
+app.patch('/', mirrow);
+app.delete('/', mirrow);
+app.head('/', mirrow);
+
+app.listen(port, () => {
+    console.log("Hola Mundo");
+});
